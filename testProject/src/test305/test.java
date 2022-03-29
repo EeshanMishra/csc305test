b71d2834008e2c64098f9400361eb1f2a1fabf74
eeshanmishra <eeshanmishra@laptop-30apo7vf.sloliving>
2022-03-28 23:43:25 -0700

    Adding a change to the test project

--- testProject/src/test305/test.java
+++ testProject/src/test305/test.java
@@ -2,6 +2,6 @@ package test305;
 
 public class test {
 	public static void main(String args[]) {
-		System.out.println("Welcome to CSC 305");
+		System.out.println("Welcome to CSC 305, but with a twist");
 	}
 }
